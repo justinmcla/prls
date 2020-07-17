04f70e6d63d4e16090c95e68c6395bc5918d96ca
justinmcla <jwmclawhorn@gmail.com>
2020-07-17 14:12:46 -0400

    revised get_details to include add_attr_array

--- lib/prls/concord.rb
+++ lib/prls/concord.rb
@@ -26,7 +26,7 @@ class PRLS::CLI::CONCORD < PRLS::CLI::PRO
 
     def self.get_details(index)
         if self.all[index].need_attr?
-            self.all[index].add_attr(PRLS::CLI::Scraper.new.concord_info(self.all[index].url))
+            self.all[index].add_attr_array(PRLS::CLI::Scraper.new.concord_info(self.all[index].url))
         end
     end
 
