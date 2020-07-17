f5b08766e3a6b2d7e16641738e72b57fc8119087
justinmcla <jwmclawhorn@gmail.com>
2020-07-17 18:26:59 -0400

    refactored bpp_index

--- lib/prls/scraper.rb
+++ lib/prls/scraper.rb
@@ -59,14 +59,7 @@ class PRLS::CLI::Scraper
 
     def bpp_index(url)
         bpp_index = Nokogiri::HTML(open(url))
-        @bpp_plays = []
-        bpp_index.css('.product-details').each do |play|
-            @bpp_plays << {
-                :title => play.css('a').text.chomp(' [read more]'),
-                :url => play.css('a').attribute('href').value
-            }
-        end
-        @bpp_plays
+        bpp_index.css('.product-details').map { |play| {:title => play.css('a').text.chomp(' [read more]'), :url => play.css('a').attribute('href').value} }
     end
 
     def bpp_info(url)
