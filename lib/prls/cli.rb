75cd67e1221ceaa7fa45dd434a70f096576b1a26
justinmcla <jwmclawhorn@gmail.com>
2020-07-18 13:46:48 -0400

    refactored cli menu

--- lib/prls/cli.rb
+++ lib/prls/cli.rb
@@ -42,33 +42,18 @@ class PRLS::CLI
         input = gets.chomp.downcase
         case input.to_s
         when '1'
-            puts ""
-            puts "Loading..."
-            puts ""
             DPS.get_plays
             DPS.list_plays
         when '2'
-            puts ""
-            puts "Loading..."
-            puts ""
             CONCORD.get_plays
             CONCORD.list_plays
         when '3'
-            puts ""
-            puts "Loading..."
-            puts ""
             MTI.get_plays
             MTI.list_plays
         when '4'
-            puts ""
-            puts "Loading..."
-            puts ""
             PLAYSCRIPTS.get_plays
             PLAYSCRIPTS.list_plays
         when '5'
-            puts ""
-            puts "Loading..."
-            puts ""
             BPP.get_plays
             BPP.list_plays
         when 'exit'
